5a42953c8e885dffb0050245b7bc7e5bcea88a69
val97p <50250788+val97p@users.noreply.github.com>
2019-11-20 21:31:43 -0500

    Add files via upload

---
+++ Sequency.py
@@ -0,0 +1,57 @@
+list = [1, 2, 4, 7, 11]
+print(list)
+
+next_number = int(input("What's the next number? "))
+
+while next_number != 16:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+
+next_number = int(input("What's the next number? "))
+
+while next_number != 22:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+
+next_number = int(input("What's the next number? "))
+
+while next_number != 29:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+
+while next_number != 37:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+
+while next_number != 46:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+
+while next_number != 56:
+    print("Wrong !!!")
+    print ("Try Again")
+    next_number = int(input("What's the next number? "))
+else:
+    list.append(next_number)
+    print(list)
+    print ("end")
